74b9b72ca91a55432dbf49d0cdfa590673e749b0
dinrol <rakov0@mail.ru>
2020-12-08 11:57:50 +0300

    first commit

---
+++ src/auth.js
@@ -0,0 +1,28 @@
+export function getAuthForm() {
+   return `
+   <form id="auth-form" class="mui-form">
+   <div class="mui-textfield mui-textfield--float-label">
+      <input type="email" id="email" required>
+      <label for="email">Email</label>
+   </div>
+   <div class="mui-textfield mui-textfield--float-label">
+      <input type="password" id="password" required>
+      <label for="password">Пароль</label>
+   </div>
+   <button type="submit" class="mui-btn mui-btn--raised mui-btn--accent">Войти</button>
+   </form>
+   `
+}
+
+export function authWithEmailAndPassword(email, password) {
+   const apiKey = 'AIzaSyCgPzZo7r_GTu-p9oDnNMqajqBjpF5UoaM'
+   return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password, returnSecureToken: true }),
+      headers: {
+         'Content-type': 'application/json'
+      }
+   })
+      .then(response => response.json())
+      .then(data => data.idToken)
+}
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+export function isValid(value) {
+   return value.length >= 10
+}
+
+export function createModal(title, content) {
+   const modalEl = document.createElement('div')
+   modalEl.classList.add('modal')
+   modalEl.innerHTML = `
+      <h1>${title}</h1>
+      <div class="modal-content">${content}</div>
+   `
+   mui.overlay('on', modalEl);
+}
